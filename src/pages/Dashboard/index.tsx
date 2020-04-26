59ec7b4f5bcaf7a7071e76b47036e0c76b66279e
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-04-25 23:11:04 -0300

    Project init, add pages, and global style

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Logo" />
+      <Title>Explore repositórios no Github</Title>
+    </>
+  );
+};
+
+export default Dashboard;
